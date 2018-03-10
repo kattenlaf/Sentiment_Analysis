d7268e44dd63edfbddc367865fd9121954019156
kattenlaf <32383214+kattenlaf@users.noreply.github.com>
2018-03-10 04:37:26 -0500

    Update Streaming.py

--- Streaming.py
+++ Streaming.py
@@ -1,39 +1,43 @@
+import data as data
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import time
 
-#Add your own consumer keys here 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+consumer_key = 'ZWXZ5yeixFdDWNkdNyk9ugNKq'
+consumer_secret = '8x3SbOD5CvybTSWCzLxJ4jAK4JxwkMCsZ4qJpmwb7q0Elu8LSe'
+access_token = '812361304640000000-PwUtZe0JkdNW0DMt00JFh612SnKCJ2O'
+access_secret = 'y5CS3IPhXjsbqIppQeX3EcMSOE8fK0yfoAQX05J87TZ2c'
 
-class listener( StreamListener ):
+class Listener( StreamListener ):
 
-	def on_data( self, data ):
-
-		try:
-			tweet = data.split(',"text":"')[1].split('","source')[0]
-			print tweet
-			saveThis = str( time.time() ) + '::' + tweet
-			saveFile = open('Test.csv', 'a')
-			saveFile.write(saveThis)
-			saveFile.write('\n')
-			saveFile.close()
-			return True
-
-		except BaseException, e:
-			print 'failed ondata,',str(e)
-			time.sleep(10)
+    def on_data( self, data ):
+        try:
+            tweet = data
+            print tweet
+            saveFile = open('Test.csv', 'a')
+            saveFile.write(tweet)
+            saveFile.write('\n')
+            saveFile.close()
+            #mydata
+            tweet2 = data.split(',"text":"')[1].split('","source')[0]
+            saveThis = str( time.time() ) + '::' + tweet
+            saveFile2 = open('ruelFile.csv', 'a')
+            saveFile2.write(saveThis)
+            saveFile2.write('\n')
+            saveFile2.close()
+            return True
+        except BaseException, e:
+            print 'failed ondata,',str(e)
+            time.sleep(10)
 
 	def on_error(self, status):
 		print status
 
 auth = OAuthHandler( consumer_key, consumer_secret )
 auth.set_access_token( access_token, access_secret )
-
-twitterStream = Stream( auth, listener() )
-twitterStream.filter(track=["phone"])
+keywords = [ "Nintendo", "Ninteno", "intendo", "Nintedo", "Nintendoi", "Nintend", "Niintendo" "iintendo"]
+twitterStream = Stream( auth, Listener() )
+twitterStream.filter(track=keywords)
 
 #Stream tweets using the keyword in python2
